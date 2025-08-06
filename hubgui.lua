0a0aea2b7c5a30d3ed5550b90c13f96af77e7dbb
julianofficial01 <deinv267@gmail.com>
2025-08-06 03:38:29 +0200

    Update hubgui.lua

--- hubgui.lua
+++ hubgui.lua
@@ -2,31 +2,76 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 
--- Erstelle das ScreenGui
 local screengui = Instance.new("ScreenGui")
 screengui.Name = "JM.Menu"
-screengui.ResetOnSpawn = false -- damit es beim Respawn nicht verschwindet
 screengui.Parent = playerGui
 
--- Erstelle das Hauptmenü-Frame
 local mainframe = Instance.new("Frame")
-mainframe.Name = "MainFrame"
+mainframe.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
 mainframe.Size = UDim2.new(0, 693, 0, 507)
 mainframe.Position = UDim2.new(0.319, 0, 0.184, 0)
-mainframe.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-mainframe.BorderSizePixel = 0
-mainframe.Active = true         -- Für Drag-Funktion
-mainframe.Draggable = true      -- Optional: Frame verschiebbar machen
-mainframe.ZIndex = 2
 mainframe.Parent = screengui
 
--- Rahmen (UIStroke)
 local mainframeStroke = Instance.new("UIStroke")
 mainframeStroke.Color = Color3.fromRGB(0, 7, 70)
 mainframeStroke.Thickness = 3
 mainframeStroke.Parent = mainframe
 
--- Abgerundete Ecken (UICorner)
 local mainframeCorner = Instance.new("UICorner")
 mainframeCorner.CornerRadius = UDim.new(0, 10)
 mainframeCorner.Parent = mainframe
+
+local mainframeListframe = Instance.new("Frame")
+mainframeListframe.BackgroundTransparency = 1
+mainframeListframe.Size = UDim2.new(0, 693, 0, 36)
+mainframeListframe.Position = UDim2.new(0, 0, 0.901, 0)
+mainframeListframe.Parent = mainframe
+
+local mainframeListframeMain = Instance.new("UIListLayout")
+mainframeListframeMain.Padding = UDim.new(0, 15)
+mainframeListframeMain.SortOrder = Enum.SortOrder.LayoutOrder
+mainframeListframeMain.FillDirection = Enum.FillDirection.Horizontal
+mainframeListframeMain.HorizontalAlignment = Enum.HorizontalAlignment.Center
+mainframeListframeMain.VerticalAlignment = Enum.VerticalAlignment.Top
+mainframeListframeMain.Parent = mainframeListframe
+
+local mainframeListframeScort1 = Instance.new("Frame")
+mainframeListframeScort1.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+mainframeListframeScort1.Size = UDim2.new(0, 79, 0, 36)
+mainframeListframeScort1.Parent = mainframeListframe
+
+local mainframeListframeScort2 = Instance.new("Frame")
+mainframeListframeScort1.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+mainframeListframeScort1.Size = UDim2.new(0, 79, 0, 36)
+mainframeListframeScort1.Parent = mainframeListframe
+
+local mainframeListframeScort3 = Instance.new("Frame")
+mainframeListframeScort1.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+mainframeListframeScort1.Size = UDim2.new(0, 79, 0, 36)
+mainframeListframeScort1.Parent = mainframeListframe
+
+local mainframeClose = Instance.new("Frame")
+mainframeClose.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+mainframeClose.Size = UDim2.new(0, 79, 0, 27)
+mainframeClose.Position = UDim2.new(0.865, 0, 0.022, 0)
+mainframeClose.Parent = mainframe
+
+local mainframeCloseText = Instance.new("TextLabel")
+mainframeCloseText.BackgroundTransparency = 1
+mainframeCloseText.Size = UDim2.new(0, 79, 0, 21)
+mainframeCloseText.Text = "Close"
+mainframeCloseText.TextColor3 = Color3.fromRGB(255, 255, 255)
+mainframeCloseText.Font = Enum.Font.Arimo
+mainframeCloseText.TextScaled = true
+mainframeCloseText.Parent = mainframeClose
+
+local mainframeTitle = Instance.new("TextLabel")
+mainframeTitle.BackgroundTransparency = 1
+mainframeTitle.TextXAlignment = Enum.TextXAlignment.Left
+mainframeTitle.Size = UDim2.new(0, 693, 0, 36)
+mainframeTitle.Position = UDim2.new(0.026, 0, 0.022, 0)
+mainframeTitle.Text = "Void Hacks"
+mainframeTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+mainframeTitle.Font = Enum.Font.Arimo
+mainframeTitle.TextScaled = true
+mainframeTitle.Parent = mainframe
