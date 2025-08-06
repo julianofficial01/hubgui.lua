fbb7711735e1e00b7cee41dffc81d923348a4c46
julianofficial01 <deinv267@gmail.com>
2025-08-06 03:56:38 +0200

    Update hubgui.lua

--- hubgui.lua
+++ hubgui.lua
@@ -76,7 +76,8 @@ mainframeCloseStroke.Parent = mainframeClose
 -- Close Button (TextButton mit fetter Schrift)
 local mainframeCloseButton = Instance.new("TextButton")
 mainframeCloseButton.BackgroundTransparency = 1
-mainframeCloseButton.Size = UDim2.new(1, 0, 1, 0)
+mainframeCloseButton.Size = UDim2.new(0, 79, 0, 27)
+mainframeCloseButton.Position = UDim2.new(0.865, 0, 0.022, 0)
 mainframeCloseButton.Text = "Close"
 mainframeCloseButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 mainframeCloseButton.FontFace = Font.fromName("Arimo", Enum.FontWeight.Bold)
@@ -102,5 +103,6 @@ mainframeTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
 mainframeTitle.FontFace = Font.fromName("Arimo", Enum.FontWeight.Bold)
 mainframeTitle.TextScaled = true
 mainframeTitle.Parent = mainframe
+
 
 
